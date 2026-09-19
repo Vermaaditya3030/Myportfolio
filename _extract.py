40f7b60871252516b8f89ca7e61178d1d3d7e44a
Aditya Verma <132653045+Vermaaditya3030@users.noreply.github.com>
2026-09-19 15:11:11 +0530

    Update _extract.py

--- _extract.py
+++ _extract.py
@@ -1,7 +1,7 @@
 import subprocess, sys
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyPDF2', '-q'])
 from PyPDF2 import PdfReader
-r = PdfReader(r'c:\Users\abhis\OneDrive\Desktop\Next_Protfolio\public\resume.pdf')
+r = PdfReader(r"C:\Users\Aditya Verma\Downloads\Portfolio-main\Portfolio-main\public\resume.pdf")
 for i, page in enumerate(r.pages):
     t = page.extract_text()
     if t:
